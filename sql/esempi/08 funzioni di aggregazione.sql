cbbc65607a771b5ec8e885b34218f1e451296d23
skid <skid@kinkpad.local>
2024-11-25 14:07:55 +0100

    nuovi esempi in TODO

--- sql/esempi/08 funzioni di aggregazione.sql
+++ sql/esempi/08 funzioni di aggregazione.sql
@@ -1,3 +1,15 @@
+-- FUNZIONI DI AGGREGAZIONE
+-- Le funzioni di aggregazione ricevono in ingresso un elenco di valori
+-- e resituiscono un unico valore.
+-- Sono spesso usate insieme a GROUP BY.
+-- Esempi di funzione di aggregazione sono:
+-- * COUNT: conteggio
+-- * MAX: calcola il massimo
+-- * MIN: calcola il minimo
+-- * SUM: calcola la somma
+-- * AVG: calcola la media
+-- * VARIANCE: calcola la varianza
+
 USE ecommerce;
 
 -- TODO
