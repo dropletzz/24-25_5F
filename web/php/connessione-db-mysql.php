0741312e0f2655cd418f3d86a8ae74cd9fb1e0d7
skid <skid@kinkpad.local>
2024-12-13 11:44:31 +0100

    primi esempi di codice php

---
+++ web/php/connessione-db-mysql.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Login</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <div class="contenitore">
+		<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "insta";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, email, first_name, last_name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  echo "<table>";
+  while($row = $result->fetch_assoc()) {
+	  echo "<tr>";
+	  echo "<td>{$row["id"]}</td>";
+	  echo "<td>{$row["email"]}</td>";
+	  echo "<td>{$row["first_name"]}</td>";
+	  echo "<td>{$row["last_name"]}</td>";
+	  echo "</tr>";}
+  echo "</table>";
+} else {
+  echo "Nessun utente nel db";
+}
+$conn->close();
+?> 
+    </div>
+</body>
+
+</html>
