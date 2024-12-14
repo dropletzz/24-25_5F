827aaa2dc16f1b47dae0173078adf18027dd56b9
skid <skid@kinkpad.local>
2024-12-14 21:41:29 +0100

    spostato sql create e fill in un unico file

--- sql/create-all.sh
+++ sql/create-all.sh
@@ -1,5 +1,3 @@
 
-mariadb -u root < /_dbdata/social/create.sql
-mariadb -u root < /_dbdata/social/fill.sql
-mariadb -u root < /_dbdata/library/create.sql
-mariadb -u root < /_dbdata/library/fill.sql
+mariadb -u root < /_dbdata/social.sql
+mariadb -u root < /_dbdata/library.sql
