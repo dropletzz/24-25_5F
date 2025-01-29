c218e9b63ab3ac137e7c5319ed528f6419efbc68
skid <skid@kinkpad.local>
2025-01-29 11:12:17 +0100

    hotfix

--- web/public/auth/index.php
+++ web/public/auth/index.php
@@ -10,8 +10,8 @@
     <?php if (isset($_GET['autenticato'])): ?>
         <h1>Benvenuto nella tua area riservata!</h1>
     <?php else: ?>
-        <p>Nuovo utente? <a href="registrazione.html">Registrati</a></p> 
-        <p>Sei gia' registrato? <a href="login.html">Fai il login</a></p> 
+        <p>Nuovo utente? <a href="registrazione.php">Registrati</a></p> 
+        <p>Sei gia' registrato? <a href="login.php">Fai il login</a></p> 
     <?php endif; ?>
 </body>
 
