1869c52f892cc881e1ee011735068835313f4163
skid <skid@aule.local>
2025-02-26 19:55:18 +0100

    fix errore in un commento

--- web/public/auth/logout.php
+++ web/public/auth/logout.php
@@ -1,5 +1,5 @@
 <?php
-    session_start(); # da fare ogni volta che usimo le sessioni
+    session_start(); # da fare ogni volta che usiamo le sessioni
     session_destroy(); # elimina la sessione corrente
     header('Location: index.php');
 ?>
